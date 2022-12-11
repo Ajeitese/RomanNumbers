6b4a4dd3598a5a07acc2e53f0d30349d1db0f94b
Victor <victormb19@gmail.com>
2022-12-11 21:06:32 +0100

    Convert 0 to Roman Done

---
+++ RomanNumbersTests/RomanNumberShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace RomanNumbersTests
+{
+    [TestFixture]
+    public class RomanNumberShould
+    {
+        RomanNumber romanNumber;
+
+        [SetUp]
+        public void Setup()
+        {
+            romanNumber = new RomanNumber();
+        }
+        
+        [Test]
+        public void Convert_To_Roman_Number()
+        {
+            Assert.That(romanNumber.ConvertToRoman(0), Is.EqualTo(string.Empty));
+        }
+
+    }
+
+    public class RomanNumber
+    {
+        public string ConvertToRoman(int number)
+        {
+            return string.Empty;
+        }
+    }
+}
