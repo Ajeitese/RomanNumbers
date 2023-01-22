c535be46cb9f2390cf6611a7fdeb5333a8722e3f
PC <ajeitese@gmail.com>
2023-01-22 22:58:04 +0100

    Add RomanNumber class

---
+++ RomanNumbers/RomanNumber.cs
@@ -0,0 +1,11 @@
+﻿namespace RomanNumbers
+{
+
+    public static class RomanNumber
+    {
+        public static string ConvertFromDecimal(int number)
+        {
+            return "";
+        }
+    }
+}
